f4aab8004354403b5572c05a15f4c241ba5b66a5
MrCherry89 <abgarnati@yandex.ru>
2024-10-22 23:47:37 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -30,6 +30,73 @@ $(document).ready(function () {
     ],
   });
 
+  $("ul.tabs li").click(function () {
+    var tab_id = $(this).attr("data-tab");
+
+    // Удаляем активные классы
+    $("ul.tabs li").removeClass("active");
+    $(".tab-content").removeClass("active");
+
+    // Добавляем активный класс для выбранной вкладки и ее контента
+    $(this).addClass("active");
+    $("#" + tab_id).addClass("active");
+  });
+
+  $(".advantages-info-slider").slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: true,
+          autoplay: true,
+          autoplaySpeed: 1000,
+          speed: 800,
+        },
+      },
+    ],
+  });
+
+  $(".wholesale-brends-slider").slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 1200,
+    speed: 800,
+  });
+
+  $(".advantages-slider").slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          speed: 800,
+        },
+      },
+    ],
+  });
+
   $(".reviews-slider").slick({
     dots: true,
     arrows: true,
@@ -63,5 +130,15 @@ $(document).ready(function () {
         },
       },
     ],
+  });
+
+  $(".see-all").on("click", function () {
+    $(this).closest(".by-brand").find(".info-items").toggleClass("show");
+    $(this).find("img").toggleClass("rotate");
+    if (!$(".info-items").hasClass("show")) {
+      $(".see-all span").text("Смотреть все");
+    } else {
+      $(".see-all span").text("Скрыть все");
+    }
   });
 });
